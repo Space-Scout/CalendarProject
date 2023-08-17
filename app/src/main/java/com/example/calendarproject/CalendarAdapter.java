bf74c5e0af96f45f31e6a6a93117d336fbd7254e
Space-Scout <benmayf343@gmail.com>
2023-08-17 18:38:18 -0400

    Few hours work

--- app/src/main/java/com/example/calendarproject/CalendarAdapter.java
+++ app/src/main/java/com/example/calendarproject/CalendarAdapter.java
@@ -1,25 +1,59 @@
 package com.example.calendarproject;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+
 public class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>{
-    @NonNull
-    @NotNull
+
+    private final ArrayList<String> daysOfMonth;
+    private final OnItemListener onItemListener;
+
+    //daysOfMonthConstructor
+    public CalendarAdapter(ArrayList<String> daysOfMonth, OnItemListener onItemListener) {
+        this.daysOfMonth = daysOfMonth;
+        this.onItemListener = onItemListener;
+    }
+
+
+    //IMPLEMENTING CalendarViewHolder methods
+    @NonNull //automatic by Java
+    @NotNull //automatic by Java
     @Override
     public CalendarViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        //The LayoutInflater class is used to instantiate the contents of layout XML files into their corresponding View objects.
+        //In other words, it takes an XML file as input and builds the View objects from it.
+        View view = inflater.inflate(R.layout.calendar_cell, parent,false);
+        //inflater object takes the calendar_cell xml file from layout and makes it a View object
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.height = (int) (parent.getHeight() * 0.166666666); //getting full height of the view and then multiply by 1/6th so each cell is 1/6th of the view
+        return new CalendarViewHolder(view, onItemListener);
     }
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull CalendarViewHolder holder, int position) {
-
+        holder.dayOfMonth.setText(daysOfMonth.get(position)); //Java utilisation of TextView
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return daysOfMonth.size(); //get size of ArrayList of daysOfMonth
     }
+
+
+    //Interface
+    public interface OnItemListener{
+        void onItemClick(int position, String dayText);
+    }
+
 }
+
+
+
+
--- app/src/main/java/com/example/calendarproject/CalendarViewHolder.java
+++ app/src/main/java/com/example/calendarproject/CalendarViewHolder.java
@@ -6,11 +6,25 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import org.jetbrains.annotations.NotNull;
 
-public class CalendarViewHolder extends RecyclerView.ViewHolder {
-    private final TextView dayOfMonth;
+public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
-    public CalendarViewHolder(@NonNull @NotNull View itemView) {
+    public final TextView dayOfMonth; // TextView displays text to the user and optionally allows them to edit it programmatically. XML(android:etc) and Java textView.setText, etc
+    private final CalendarAdapter.OnItemListener onItemListener;
+
+
+    public CalendarViewHolder(@NonNull @NotNull View itemView, CalendarAdapter.OnItemListener onItemListener) {
         super(itemView);
         dayOfMonth = itemView.findViewById(R.id.cellDayText);
+        //retrieves text with id "cellDayText" from calendar_cell.xml which is now a View object due to the CalendarViewHolder method
+        //in CalendarAdapter changing the xml into a View object by using inflater.
+        this.onItemListener = onItemListener;
+        itemView.setOnClickListener(this);
+    }
+
+    @Override //Implemented from View.OnClickListener interface from View.class
+    public void onClick(View view) {
+        onItemListener.onItemClick(getAdapterPosition(), (String)dayOfMonth.getText());
+        //View object is passed in containing click position in the view object
+        //casting the TextView dayOfMonth to a string after retrieving ~ android:text="" ~ field from TextView object
     }
 }
